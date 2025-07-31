cba6d6bc5137890ff1ba9148a0a5c1e16b093e76
Pasupathi M <110220165+Pasupathi21@users.noreply.github.com>
2025-07-31 22:10:07 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
-import { lazy, Suspense } from 'react'
-import { createBrowserRouter, type RouteObject } from 'react-router-dom'
+import { lazy } from 'react'
 import { AppRoutes } from '@/app-constant/app-routes'
 import { type TNestedObj } from '@/types/global.types'
 import { type TRouteObject } from '@/types/route.type'
 import { mergeRoutes } from '@/routes/route-util'
 
 // pages
+// import { Layout } from '@/layout/index'
 const SignInSignUp = lazy(() => import('@/pages/signin-signup'))
-const Chat = lazy(() => import('@/pages/chat'))
+const Chat = lazy(() => import('@/pages/apps/chat'))
+const Layout = lazy(() => import('@/layout/index'))
+
 
 const publicRoutes:TRouteObject[]  = [
     {
@@ -23,19 +25,28 @@ const publicRoutes:TRouteObject[]  = [
 ]
 const childRoutes: TRouteObject[] = [
     {
-        path: AppRoutes.PRIVATE.app.chat
+        path: AppRoutes.PRIVATE.app.chat,
+        element: <Chat />,
+        isLazy: true
     }
 ] 
 const protectedRoutes: TRouteObject[] = [
     {
         path: AppRoutes.PRIVATE.app.index,
-        element: <Chat />,
-        isLazy: false,
+        element: <Layout />,
+        isLazy: true,
         children: childRoutes
     }
 ]
 
+const errorRoutes: TRouteObject[] = [
+    {
+        path: ''
+    }
+]
+
 export const GET_APP_ROUTES = (__props: TNestedObj = {}): any => {
-    const getMergeRoutes = mergeRoutes([...publicRoutes, ...protectedRoutes])
+    const getMergeRoutes = mergeRoutes([...publicRoutes, ...protectedRoutes, ...errorRoutes])
+    console.log(getMergeRoutes)
     return  getMergeRoutes
 }
