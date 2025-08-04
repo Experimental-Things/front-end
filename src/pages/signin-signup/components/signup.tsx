2bf493fafb18ea76979548373cca5a25d853bbbc
Pasupathi M <110220165+Pasupathi21@users.noreply.github.com>
2025-08-04 22:39:15 +0530

    Add files via upload

---
+++ src/pages/signin-signup/components/signup.tsx
@@ -0,0 +1,48 @@
+import LoginImage from '@/assets/images/login.webp'
+
+export default function SignUp() {
+    return (
+        <>
+            <form className="space-y-4">
+                <div>
+                    <label className="block text-sm font-medium mb-1">Email</label>
+                    <input
+                        type="email"
+                        placeholder="you@example.com"
+                        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                </div>
+                <div>
+                    <label className="block text-sm font-medium mb-1">Password</label>
+                    <input
+                        type="password"
+                        placeholder="••••••••"
+                        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                </div>
+                <div>
+                    <label className="block text-sm font-medium mb-1">Password</label>
+                    <input
+                        type="password"
+                        placeholder="••••••••"
+                        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                </div>
+                <div>
+                    <label className="block text-sm font-medium mb-1">Password</label>
+                    <input
+                        type="password"
+                        placeholder="••••••••"
+                        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                </div>
+                <button
+                    type="submit"
+                    className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
+                >
+                    Continue
+                </button>
+            </form>
+        </>
+    )
+}
